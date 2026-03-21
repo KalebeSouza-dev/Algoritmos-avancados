fd4f2e3b7c45a9b7d3a98a3163d0da19792ca022
KalebeSouza-dev <kalebesouza.cc@gmail.com>
2026-03-21 19:09:42 -0300

    remove .cph files

---
+++ all-in/2000+/1455E.cpp
@@ -0,0 +1,14 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define endl '\n'
+#define rep(i, a, b) for (int i = a; i < (b); i++)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    
+    
+}
